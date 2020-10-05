824d2912391755607438f6684474f7a7544705db
manuelvargastapia <manuelvargastapia@gmail.com>
2020-10-05 20:27:08 -0300

    implement downloading with flutter_downloader

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 
 import 'src/app.dart';
 
-void main() {
+void main() async {
+  // Unique initialization required
+  await FlutterDownloader.initialize(debug: true);
+
   runApp(LibGen());
 }
--- lib/src/blocs/book_block.dart
+++ lib/src/blocs/book_block.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
+import 'package:downloads_path_provider/downloads_path_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:libgen/src/models/downloadLink.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:rxdart/rxdart.dart';
 
 import '../models/result.dart';
@@ -16,9 +22,20 @@ class BookBloc {
   }
 
   downloadBook(String md5) async {
+    await Permission.storage.request();
     DownloadLink downloadLink = await _repository.getDownloadLink(md5);
-    // TODO: download
-    print(downloadLink.url);
+    Directory downloadsDirectory =
+        await DownloadsPathProvider.downloadsDirectory;
+    WidgetsFlutterBinding
+        .ensureInitialized(); // Check FlutterDownloader initialization
+    if (await Permission.storage.isGranted) {
+      await FlutterDownloader.enqueue(
+        url: downloadLink.url,
+        savedDir: downloadsDirectory.path,
+        showNotification: true,
+        openFileFromNotification: true,
+      );
+    }
   }
 
   dispose() {
