68514aad1d10ce034ee8413014145abbd4950956
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-07 13:16:12 -0300

    create DisplayBookScreen

---
+++ lib/src/feature_display_book/book_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:libgen/src/feature_display_book/widgets/book_body.dart';
+
+class DisplayBookScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BookBody(),
+    );
+  }
+}
