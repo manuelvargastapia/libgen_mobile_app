2c975cbb70d1c218eb7d3fa5cb21ca49c0910b9f
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-07 13:19:13 -0300

    create BookModel + autogenerate code from model with build_runner and json_serializable

---
+++ lib/src/feature_display_book/models/book_model.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'book_model.g.dart';
+
+@JsonSerializable()
+class BookModel {
+  int id;
+  String title;
+  String author;
+  int year;
+  String md5;
+  String coverUrl;
+
+  BookModel(
+      {@required this.id,
+      @required this.title,
+      @required this.author,
+      @required this.year,
+      @required this.md5,
+      @required this.coverUrl});
+
+  factory BookModel.fromJson(Map<String, dynamic> json) =>
+      _$BookModelFromJson(json);
+}
