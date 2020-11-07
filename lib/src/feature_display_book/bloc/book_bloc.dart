38a9b13edd0f04018de4fa7939f311af53193eeb
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-07 13:17:06 -0300

    create BookBloc to handle complex logic and generate states for UI

---
+++ lib/src/feature_display_book/bloc/book_bloc.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'package:libgen/src/feature_display_book/bloc/book_event.dart';
+import 'package:libgen/src/feature_display_book/bloc/book_state.dart';
+import 'package:libgen/src/feature_display_book/models/book_model.dart';
+import 'package:libgen/src/feature_display_book/models/download_link_model.dart';
+import 'package:libgen/src/feature_display_book/repository/book_repository.dart';
+
+import 'package:downloads_path_provider/downloads_path_provider.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class BookBloc extends Bloc<BookEvent, BookState> {
+  final BookRepository bookRepository;
+  bool isFetching = false;
+  bool isDownloading = false;
+
+  BookBloc({@required this.bookRepository}) : super(BookInitialState());
+
+  @override
+  Stream<BookState> mapEventToState(BookEvent event) async* {
+    if (event is BookFetchEvent) {
+      yield BookLoadingState(message: 'Loading books');
+      final response = await bookRepository.getBooks(
+        searchQuery: event.query,
+        offset: event.currentOffset,
+      );
+      if (response is http.Response) {
+        if (response.statusCode == 200) {
+          final rawBooks = jsonDecode(response.body)['data'] as List;
+          final List<BookModel> books = [];
+          rawBooks.forEach((book) {
+            books.add(BookModel.fromJson(book));
+          });
+          yield BookSuccessState(books: books);
+        } else {
+          yield BookErrorState(error: response.body);
+        }
+      } else if (response is String) {
+        yield BookErrorState(error: response);
+      }
+    } else if (event is DownloadBookEvent) {
+      yield DownloadInProgress(message: 'Downloading book', md5: event.md5);
+      final response = await bookRepository.getDownloadLink(md5: event.md5);
+      if (response is http.Response) {
+        if (response.statusCode == 200) {
+          final String downloadLink =
+              DownloadLinkModel.fromJson(jsonDecode(response.body)['data'])
+                  .downloadLink;
+          yield DownloadSuccessful(message: downloadLink, md5: event.md5);
+          await Permission.storage.request();
+          Directory downloadsDirectory =
+              await DownloadsPathProvider.downloadsDirectory;
+          WidgetsFlutterBinding.ensureInitialized();
+          if (await Permission.storage.isGranted) {
+            await FlutterDownloader.enqueue(
+              url: downloadLink,
+              savedDir: downloadsDirectory.path,
+              showNotification: true,
+              openFileFromNotification: true,
+            );
+          }
+        } else {
+          yield DownloadError(error: response.body, md5: event.md5);
+        }
+      } else if (response is String) {
+        yield DownloadError(error: response, md5: event.md5);
+      }
+    }
+  }
+}
