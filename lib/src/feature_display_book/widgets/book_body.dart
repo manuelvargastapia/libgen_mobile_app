8d1b3596749f2c19fb2e6ac78b5eaa5851788628
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-09 19:26:06 -0300

    WIP: update general UI workflow to incorporate a filter dialog before searching

--- lib/src/feature_display_book/widgets/book_body.dart
+++ lib/src/feature_display_book/widgets/book_body.dart
@@ -4,11 +4,34 @@ import 'package:libgen/src/feature_display_book/bloc/book_bloc.dart';
 import 'package:libgen/src/feature_display_book/bloc/book_event.dart';
 import 'package:libgen/src/feature_display_book/bloc/book_state.dart';
 import 'package:libgen/src/feature_display_book/models/book_model.dart';
+import 'package:libgen/src/feature_display_book/models/search_query_model.dart';
 import 'package:libgen/src/feature_display_book/repository/book_repository.dart';
 
 import 'book_list_item.dart';
 
-class BookBody extends StatelessWidget {
+class BookList extends StatefulWidget {
+  @override
+  _BookListState createState() => _BookListState();
+}
+
+class _BookListState extends State<BookList> {
+  String searchIn;
+  String sortBy;
+
+  @override
+  void initState() {
+    super.initState();
+    searchIn = 'def';
+    sortBy = 'def';
+  }
+
+  void updateSearchQuery(String newSearchIn, String newSortBy) {
+    setState(() {
+      searchIn = newSearchIn;
+      sortBy = newSortBy;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
@@ -23,8 +46,17 @@ class BookBody extends StatelessWidget {
               onPressed: () {
                 showSearch(
                   context: context,
-                  delegate: BookSearchDelegate(),
+                  delegate: BookSearchDelegate(
+                    searchIn: searchIn,
+                    sortBy: sortBy,
+                  ),
                 );
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.filter_list),
+              onPressed: () {
+                showFilterDialog(context, updateSearchQuery);
               },
             )
           ],
@@ -34,9 +66,145 @@ class BookBody extends StatelessWidget {
   }
 }
 
+Future<void> showFilterDialog(
+  BuildContext context,
+  void Function(String newSearchIn, String newSortBy) updateStateCallback,
+) {
+  Map<String, String> _sortByValues = {
+    'def': 'Default',
+    'title': 'Title',
+    'publisher': 'Publisher',
+    'year': 'Year',
+    'pages': 'Pages',
+    'language': 'Language',
+    'filesize': 'File size',
+    'extension': 'File extension',
+  };
+
+  Map<String, String> _searchInValues = {
+    'def': 'Default',
+    'title': 'Title',
+    'author': 'Author',
+    'series': 'Series',
+    'publisher': 'Publisher',
+    'year': 'Year',
+    'identifier': 'ISBN',
+    'md5': 'MD5',
+    'extension': 'File extension'
+  };
+
+  String _sortBy = _sortByValues['def'];
+  String _searchIn = _searchInValues['def'];
+
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return AlertDialog(
+            title: Center(child: Text("Filter")),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 12, right: 10),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 16.0),
+                          child: Text("Sort by"),
+                        ),
+                        Expanded(
+                          child: DropdownButtonHideUnderline(
+                            child: DropdownButton(
+                              isExpanded: true,
+                              hint: Text("Sort by"),
+                              items: _sortByValues.values
+                                  .map(
+                                    (String value) => DropdownMenuItem(
+                                      value: value,
+                                      child: Text(value),
+                                    ),
+                                  )
+                                  .toList(),
+                              value: _sortBy,
+                              onChanged: (String newValue) {
+                                setState(() {
+                                  _sortBy = newValue;
+                                });
+                              },
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 8, right: 10),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 16.0),
+                          child: Text('Search in'),
+                        ),
+                        Expanded(
+                          child: DropdownButtonHideUnderline(
+                            child: DropdownButton(
+                              isExpanded: true,
+                              hint: Text("Search in"),
+                              items: _searchInValues.values
+                                  .map(
+                                    (String value) => DropdownMenuItem(
+                                      value: value,
+                                      child: Text(value),
+                                    ),
+                                  )
+                                  .toList(),
+                              value: _searchIn,
+                              onChanged: (String newValue) {
+                                setState(() {
+                                  _searchIn = newValue;
+                                });
+                              },
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Center(
+                    child: RaisedButton(
+                        child: Text('Apply'),
+                        onPressed: () {
+                          String sortByKey = _sortByValues.keys.firstWhere(
+                            (key) => _sortByValues[key] == _sortBy,
+                            orElse: () => null,
+                          );
+                          String searchInKey = _searchInValues.keys.firstWhere(
+                            (key) => _searchInValues[key] == _searchIn,
+                            orElse: () => null,
+                          );
+                          updateStateCallback(searchInKey, sortByKey);
+                        }),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    },
+  );
+}
+
 class BookSearchDelegate extends SearchDelegate {
   final List<BookModel> _books = [];
   final ScrollController _scrollController = ScrollController();
+
+  String searchIn;
+  String sortBy;
+
+  BookSearchDelegate({@required this.searchIn, @required this.sortBy});
 
   @override
   ThemeData appBarTheme(BuildContext context) {
@@ -89,7 +257,11 @@ class BookSearchDelegate extends SearchDelegate {
 
     return BlocProvider(
       create: (context) => BookBloc(bookRepository: BookRepository())
-        ..add(BookFetchEvent(currentOffset: 0, query: query)),
+        ..add(BookFetchEvent(SearchQueryModel(
+          searchTerm: query,
+          searchIn: this.searchIn,
+          sortBy: this.sortBy,
+        ))),
       child: Center(
         child: BlocConsumer<BookBloc, BookState>(
           listener: (context, bookState) {
@@ -124,8 +296,16 @@ class BookSearchDelegate extends SearchDelegate {
                     onPressed: () {
                       context.bloc<BookBloc>()
                         ..isFetching = true
-                        ..add(BookFetchEvent(
-                            currentOffset: _books.length, query: query));
+                        ..add(
+                          BookFetchEvent(
+                            SearchQueryModel(
+                              searchTerm: query,
+                              offset: _books.length,
+                              searchIn: this.searchIn,
+                              sortBy: this.sortBy,
+                            ),
+                          ),
+                        );
                     },
                     icon: Icon(Icons.refresh),
                   ),
@@ -142,8 +322,16 @@ class BookSearchDelegate extends SearchDelegate {
                       !context.bloc<BookBloc>().isFetching) {
                     context.bloc<BookBloc>()
                       ..isFetching = true
-                      ..add(BookFetchEvent(
-                          currentOffset: _books.length, query: query));
+                      ..add(
+                        BookFetchEvent(
+                          SearchQueryModel(
+                            searchTerm: query,
+                            offset: _books.length,
+                            searchIn: this.searchIn,
+                            sortBy: this.sortBy,
+                          ),
+                        ),
+                      );
                   }
                 }),
               itemBuilder: (context, index) => BookListItem(_books[index]),
