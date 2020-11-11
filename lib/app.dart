76a80683075a4ee2f0931bb945c29b7c6f2d5bb5
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-11 12:52:35 -0300

    make BookBloc globally available

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:libgen/src/feature_display_book/bloc/book_bloc.dart';
 import 'package:libgen/src/feature_display_book/display_book_screen.dart';
+import 'package:libgen/src/feature_display_book/repository/book_repository.dart';
 
 class LibGenApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Lib Gen App',
-      theme: ThemeData.dark(),
-      home: DisplayBookScreen(),
+    return BlocProvider(
+      create: (context) => BookBloc(bookRepository: BookRepository()),
+      child: MaterialApp(
+        title: 'Lib Gen App',
+        theme: ThemeData.dark(),
+        home: DisplayBookScreen(),
+      ),
     );
   }
 }
