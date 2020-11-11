54582a065415a31dd05ed5a2f8a7f7f7cef11a0f
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-11 12:53:03 -0300

    call already create instance of BookBloc

--- lib/src/feature_display_book/widgets/book_list/results_builder.dart
+++ lib/src/feature_display_book/widgets/book_list/results_builder.dart
@@ -5,7 +5,6 @@ import 'package:libgen/src/feature_display_book/bloc/book_event.dart';
 import 'package:libgen/src/feature_display_book/bloc/book_state.dart';
 import 'package:libgen/src/feature_display_book/models/book_model.dart';
 import 'package:libgen/src/feature_display_book/models/search_query_model.dart';
-import 'package:libgen/src/feature_display_book/repository/book_repository.dart';
 import 'package:libgen/src/feature_display_book/widgets/book_list_item/book_list_item.dart';
 
 class ResultsBuilder extends StatelessWidget {
@@ -23,74 +22,49 @@ class ResultsBuilder extends StatelessWidget {
   Widget build(BuildContext context) {
     _books.clear();
 
-    return BlocProvider(
-      create: (context) => BookBloc(bookRepository: BookRepository())
-        ..add(BookFetchEvent(SearchQueryModel(
+    final BookBloc _bookBloc = BlocProvider.of<BookBloc>(context)
+      ..add(BookFetchEvent(
+        SearchQueryModel(
           searchTerm: query,
           searchIn: this.searchIn,
           sortBy: this.sortBy,
-        ))),
-      child: Center(
-        child: BlocConsumer<BookBloc, BookState>(
-          listener: (context, bookState) {
-            if (bookState is BookLoadingState) {
-              Scaffold.of(context).showSnackBar(
-                SnackBar(content: Text(bookState.message)),
-              );
-            } else if (bookState is BookSuccessState &&
-                bookState.books.isEmpty) {
-              Scaffold.of(context).showSnackBar(
-                SnackBar(content: Text('No more books')),
-              );
-            } else if (bookState is BookErrorState) {
-              Scaffold.of(context).showSnackBar(
-                SnackBar(content: Text(bookState.error)),
-              );
-              context.bloc<BookBloc>().isFetching = false;
-            }
-          },
-          builder: (context, bookState) {
-            if (bookState is BookInitialState ||
-                bookState is BookLoadingState && _books.isEmpty) {
-              return CircularProgressIndicator();
-            } else if (bookState is BookSuccessState) {
-              _books.addAll(bookState.books);
-              context.bloc<BookBloc>().isFetching = false;
-              Scaffold.of(context).hideCurrentSnackBar();
-            } else if (bookState is BookErrorState && _books.isEmpty) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      context.bloc<BookBloc>()
-                        ..isFetching = true
-                        ..add(
-                          BookFetchEvent(
-                            SearchQueryModel(
-                              searchTerm: query,
-                              offset: _books.length,
-                              searchIn: this.searchIn,
-                              sortBy: this.sortBy,
-                            ),
-                          ),
-                        );
-                    },
-                    icon: Icon(Icons.refresh),
-                  ),
-                  const SizedBox(height: 15),
-                  Text(bookState.error, textAlign: TextAlign.center),
-                ],
-              );
-            }
-            return ListView.separated(
-              controller: _scrollController
-                ..addListener(() {
-                  if (_scrollController.offset ==
-                          _scrollController.position.maxScrollExtent &&
-                      !context.bloc<BookBloc>().isFetching) {
-                    context.bloc<BookBloc>()
+        ),
+      ));
+
+    return Center(
+      child: BlocConsumer<BookBloc, BookState>(
+        listener: (context, bookState) {
+          if (bookState is BookLoadingState) {
+            Scaffold.of(context).showSnackBar(
+              SnackBar(content: Text(bookState.message)),
+            );
+          } else if (bookState is BookSuccessState && bookState.books.isEmpty) {
+            Scaffold.of(context).showSnackBar(
+              SnackBar(content: Text('No more books')),
+            );
+          } else if (bookState is BookErrorState) {
+            Scaffold.of(context).showSnackBar(
+              SnackBar(content: Text(bookState.error)),
+            );
+            _bookBloc.isFetching = false;
+          }
+        },
+        builder: (context, bookState) {
+          if (bookState is BookInitialState ||
+              bookState is BookLoadingState && _books.isEmpty) {
+            return CircularProgressIndicator();
+          } else if (bookState is BookSuccessState) {
+            _books.addAll(bookState.books);
+            _bookBloc.isFetching = false;
+            Scaffold.of(context).hideCurrentSnackBar();
+          } else if (bookState is BookErrorState && _books.isEmpty) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    _bookBloc
                       ..isFetching = true
                       ..add(
                         BookFetchEvent(
@@ -102,14 +76,39 @@ class ResultsBuilder extends StatelessWidget {
                           ),
                         ),
                       );
-                  }
-                }),
-              itemBuilder: (context, index) => BookListItem(_books[index]),
-              separatorBuilder: (context, index) => SizedBox(height: 20),
-              itemCount: _books.length,
+                  },
+                  icon: Icon(Icons.refresh),
+                ),
+                const SizedBox(height: 15),
+                Text(bookState.error, textAlign: TextAlign.center),
+              ],
             );
-          },
-        ),
+          }
+          return ListView.separated(
+            controller: _scrollController
+              ..addListener(() {
+                if (_scrollController.offset ==
+                        _scrollController.position.maxScrollExtent &&
+                    !_bookBloc.isFetching) {
+                  _bookBloc
+                    ..isFetching = true
+                    ..add(
+                      BookFetchEvent(
+                        SearchQueryModel(
+                          searchTerm: query,
+                          offset: _books.length,
+                          searchIn: this.searchIn,
+                          sortBy: this.sortBy,
+                        ),
+                      ),
+                    );
+                }
+              }),
+            itemBuilder: (context, index) => BookListItem(_books[index]),
+            separatorBuilder: (context, index) => SizedBox(height: 20),
+            itemCount: _books.length,
+          );
+        },
       ),
     );
   }
