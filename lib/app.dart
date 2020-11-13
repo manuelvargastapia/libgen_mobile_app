4d8696e09c5273573fcab5f3786da40f07587a23
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-13 09:06:22 -0300

    change file and class name

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:libgen/src/feature_search_book/bloc/book_bloc.dart';
-import 'package:libgen/src/feature_search_book/display_book_screen.dart';
+import 'package:libgen/src/feature_search_book/search_book_screen.dart';
 import 'package:libgen/src/feature_search_book/repository/book_repository.dart';
 
 class LibGenApp extends StatelessWidget {
@@ -12,7 +12,7 @@ class LibGenApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Lib Gen App',
         theme: ThemeData.dark(),
-        home: DisplayBookScreen(),
+        home: SearchBookScreen(),
       ),
     );
   }
--- lib/src/feature_search_book/search_book_screen.dart
+++ lib/src/feature_search_book/search_book_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:libgen/src/feature_search_book/widgets/book_list/book_list.dart';
 
-class DisplayBookScreen extends StatelessWidget {
+class SearchBookScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
