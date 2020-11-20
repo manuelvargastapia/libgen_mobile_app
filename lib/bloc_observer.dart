6750cd591b8bcbab6500d9e3bbaf7778c544d2fc
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-20 14:12:33 -0300

    observe all blocs

--- lib/bloc_observer.dart
+++ lib/bloc_observer.dart
@@ -1,27 +1,27 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class LibGenBlocObserver extends BlocObserver {
+class GlobalBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object event) {
     super.onEvent(bloc, event);
-    print(event);
+    print('onEvent -- bloc: ${bloc.runtimeType}, event: $event');
   }
 
   @override
   void onChange(Cubit cubit, Change change) {
     super.onChange(cubit, change);
-    print(change);
+    print('onChange -- cubit: ${cubit.runtimeType}, change: $change');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    print(transition);
+    print('onTransition -- bloc: ${bloc.runtimeType}, transition: $transition');
   }
 
   @override
   void onError(Cubit cubit, Object error, StackTrace stackTrace) {
+    print('onError -- cubit: ${cubit.runtimeType}, error: $error');
     super.onError(cubit, error, stackTrace);
-    print('$error, $stackTrace');
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:libgen/bloc_observer.dart';
 
 import 'app.dart';
 import 'domain/suggestion.dart';
@@ -11,6 +13,8 @@ void main() async {
   await FlutterDownloader.initialize(debug: true);
   await Hive.initFlutter();
   Hive.registerAdapter(SuggestionAdapter());
+
+  Bloc.observer = GlobalBlocObserver();
 
   runApp(LibGenApp());
 }
