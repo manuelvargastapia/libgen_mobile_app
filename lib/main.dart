914bf7add59a209836a20cdafd1a60ab6737c4d8
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-22 19:27:40 -0300

    remove bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:libgen/bloc_observer.dart';
 
 import 'app.dart';
 import 'domain/suggestion.dart';
@@ -13,8 +11,6 @@ void main() async {
   await FlutterDownloader.initialize(debug: true);
   await Hive.initFlutter();
   Hive.registerAdapter(SuggestionAdapter());
-
-  Bloc.observer = GlobalBlocObserver();
 
   runApp(LibGenApp());
 }
