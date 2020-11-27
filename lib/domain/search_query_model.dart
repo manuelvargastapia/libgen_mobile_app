3e1aa396b280f19479fd7f88c567cf695ba26321
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-27 19:51:37 -0300

    make class equatable and immutable

--- lib/domain/search_query_model.dart
+++ lib/domain/search_query_model.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:equatable/equatable.dart';
 
 import 'filters_model.dart';
 
-class SearchQueryModel {
-  String searchTerm;
-  FiltersModel filters;
-  int offset;
+class SearchQueryModel extends Equatable {
+  final String searchTerm;
+  final FiltersModel filters;
+  final int offset;
 
   SearchQueryModel({
     @required this.searchTerm,
     @required this.filters,
     this.offset = 0,
   });
+
+  @override
+  List<Object> get props => [searchTerm, filters, offset];
 }
