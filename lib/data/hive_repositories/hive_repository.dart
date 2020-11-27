a114c7a710c8a6cc6b927cad97510224d7bc2adc
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-27 19:36:29 -0300

    minor formatting

--- lib/data/hive_repositories/hive_repository.dart
+++ lib/data/hive_repositories/hive_repository.dart
@@ -5,7 +5,5 @@ abstract class HiveRepository<T> {
   Either<String, T> get(Box<T> box, int index);
   Future<Either<String, void>> add(Box<T> box, T object);
   Future<Either<String, void>> delete(Box<T> box, int index);
-  Future<Either<String, int>> clear(
-    Box<T> box,
-  );
+  Future<Either<String, int>> clear(Box<T> box);
 }
