745ca8911cad0529a05afe03245b484aec98adff
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-28 19:04:37 -0300

    generate file name + refactor

--- lib/blocs/download_bloc.dart
+++ lib/blocs/download_bloc.dart
@@ -9,6 +9,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:http/http.dart' as http;
 import 'package:libgen/data/download_repository.dart';
+import 'package:libgen/domain/book_model.dart';
 import 'package:libgen/domain/task_info_model.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -72,44 +73,58 @@ class DownloadBloc extends Bloc<DownloadEvent, DownloadState> {
     IsolateNameServer.removePortNameMapping('downloader_send_port');
   }
 
+  String _generateFileName(BookModel book) {
+    final title = book.title ?? book.series ?? book.isbn ?? "untitled";
+    final author = book.author != null ? " - " + book.author : "";
+    final publisher = book.publisher != null ? " - " + book.publisher : "";
+    final year = book.year != null ? " - " + book.year.toString() : "";
+    final fileExtension =
+        book.fileExtension != null ? "." + book.fileExtension : "";
+    return "$title$author$publisher$year$fileExtension";
+  }
+
   @override
   Stream<DownloadState> mapEventToState(DownloadEvent event) async* {
-    bool _permissionGranted = false;
-    final _status = await Permission.storage.status;
+    if (event is DownloadBookEvent) {
+      bool _permissionGranted = false;
+      final _status = await Permission.storage.status;
 
-    switch (_status) {
-      case PermissionStatus.granted:
-        _permissionGranted = true;
-        break;
-      case PermissionStatus.permanentlyDenied:
-        yield DownloadPermissionsPermanentlyDenied();
-        break;
-      default:
-        final status = await Permission.storage.request();
-        _permissionGranted = status == PermissionStatus.granted;
-        break;
-    }
+      switch (_status) {
+        case PermissionStatus.granted:
+          _permissionGranted = true;
+          break;
+        case PermissionStatus.permanentlyDenied:
+          yield DownloadPermissionsPermanentlyDenied();
+          break;
+        default:
+          final status = await Permission.storage.request();
+          _permissionGranted = status == PermissionStatus.granted;
+          break;
+      }
 
-    if (event is DownloadBookEvent && _permissionGranted) {
-      yield DownloadStarting();
-      final _response = await bookRepository.getDownloadLink(event.md5);
-      if (_response is http.Response) {
-        if (_response.statusCode == 200) {
-          final String _downloadLink = DownloadLinkModel.fromJson(
-            jsonDecode(_response.body)['data'],
-          ).downloadLink;
-          Directory downloadsDirectory =
-              await DownloadsPathProvider.downloadsDirectory;
-          downloadRepository.requestDownload(
-            task: TaskInfo(name: event.md5, link: _downloadLink),
-            downloadPath: downloadsDirectory.path,
-          );
-          yield DownloadInProgress(message: "Download in progress");
-        } else {
+      if (_permissionGranted) {
+        yield DownloadStarting();
+        final _response = await bookRepository.getDownloadLink(event.book.md5);
+        if (_response is http.Response) {
+          if (_response.statusCode == 200) {
+            final String _downloadLink = DownloadLinkModel.fromJson(
+              jsonDecode(_response.body)['data'],
+            ).downloadLink;
+            Directory downloadsDirectory =
+                await DownloadsPathProvider.downloadsDirectory;
+            final String fileName = _generateFileName(event.book);
+            downloadRepository.requestDownload(
+              task: TaskInfo(name: fileName, link: _downloadLink),
+              downloadPath: downloadsDirectory.path,
+            );
+            yield DownloadInProgress(message: "Download in progress");
+          } else {
+            yield DownloadError(
+                error: "Download error. Try again later, please");
+          }
+        } else if (_response is String) {
           yield DownloadError(error: "Download error. Try again later, please");
         }
-      } else if (_response is String) {
-        yield DownloadError(error: "Download error. Try again later, please");
       }
     }
   }
