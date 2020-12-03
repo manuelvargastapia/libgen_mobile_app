74cbc752a36daf34e822644664a8a94833b82ad8
manuelvargastapia <manuelvargastapia@gmail.com>
2020-12-03 18:08:15 -0300

    UI for theme switching

--- lib/screens/search_book/search_book_screen.dart
+++ lib/screens/search_book/search_book_screen.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:libgen/blocs/book_bloc.dart';
+import 'package:libgen/blocs/events/theme_events.dart';
 import 'package:libgen/blocs/hive_bloc.dart';
+import 'package:libgen/blocs/theme_bloc.dart';
 import 'package:libgen/domain/suggestion.dart';
+import 'package:libgen/global/theme/themes.dart';
 import 'widgets/book_list/book_search_delegate.dart';
 
 class SearchBookScreen extends StatelessWidget {
@@ -66,6 +69,53 @@ class SearchBookScreen extends StatelessWidget {
               ),
             ],
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.brightness_4,
+            size: 30,
+          ),
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return Container(
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: ElevatedButton(
+                            child: Text("Light Mode"),
+                            onPressed: () {
+                              BlocProvider.of<ThemeBloc>(context).add(
+                                ChangeTheme(AppTheme.BlueLight),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: ElevatedButton(
+                            child: Text("Dark Mode"),
+                            onPressed: () {
+                              BlocProvider.of<ThemeBloc>(context).add(
+                                ChangeTheme(AppTheme.BlueDark),
+                              );
+                            },
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+            );
+          },
         ),
       ),
     );
