d681d784411ae1162abc2c2800e86c8e60dd1db2
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-01 18:14:02 -0400

    split repository into getSciTechBooks and getFictionBooks

--- lib/data/book_repository.dart
+++ lib/data/book_repository.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 
 import 'package:libgen/domain/filters_extensions.dart';
+import 'package:libgen/domain/filters_fiction_model.dart';
+import 'package:libgen/domain/filters_sci_tech_model.dart';
 import 'package:libgen/domain/search_query_model.dart';
 import 'package:libgen/global/environment/environment_config.dart';
 
@@ -20,13 +22,16 @@ class BookRepository {
   http.Response _cachedResponse;
   SearchQueryModel _cachedQuery;
 
-  Future<dynamic> getBooks(SearchQueryModel searchQuery) async {
+  Future<dynamic> getSciTechBooks(SearchQueryModel searchQuery) async {
     try {
       if (_cachedQuery == searchQuery && _cachedResponse.statusCode == 200) {
         return _cachedResponse;
       }
+      final sciTechFilters = searchQuery.filters as FiltersSciTechModel;
       final response = await http.get(
-        '$_apiURL/search?searchTerm=${searchQuery.searchTerm}&offset=${searchQuery.offset}&count=$count&searchIn=${searchQuery.filters.searchIn.displayAPILabel}&sortBy=${searchQuery.filters.sortBy.displayAPILabel}&reverse=${searchQuery.filters.reverseOrder.value}',
+        Uri.parse(
+          '$_apiURL/search?searchTerm=${searchQuery.searchTerm}&offset=${searchQuery.offset}&count=$count&searchIn=${searchQuery.filters.searchIn.displayAPILabel}&sortBy=${searchQuery.filters.sortBy.displayAPILabel}&reverse=${sciTechFilters.reverseOrder.value}',
+        ),
       );
       if (_cachedQuery != searchQuery) {
         _cachedQuery = searchQuery;
@@ -39,9 +44,37 @@ class BookRepository {
     }
   }
 
-  Future<dynamic> getDownloadLink(String md5) async {
+  Future<dynamic> getFictionBooks(SearchQueryModel searchQuery) async {
     try {
-      return await http.get('$_apiURL/download?md5=$md5');
+      if (_cachedQuery == searchQuery && _cachedResponse.statusCode == 200) {
+        return _cachedResponse;
+      }
+      final fictionFilters = searchQuery.filters as FiltersFictionModel;
+      final response = await http.get(
+        Uri.parse(
+          '$_apiURL/search/fiction?searchTerm=${searchQuery.searchTerm}&count=$count&searchIn=${searchQuery.filters.searchIn.displayAPILabel}&offset=${searchQuery.offset}&wildcardWords=${fictionFilters.wildcardWords.value}&language=${fictionFilters.language.displayAPILabel}&extension=${fictionFilters.fileExtension.displayAPILabel}',
+        ),
+      );
+      if (_cachedQuery != searchQuery) {
+        _cachedQuery = searchQuery;
+        _cachedResponse = response;
+        return _cachedResponse;
+      }
+      return response;
+    } catch (e) {
+      return e;
+    }
+  }
+
+  Future<dynamic> getDownloadLink({String md5, String downloadPageURL}) async {
+    try {
+      if (downloadPageURL != null) {
+        return await http.get(
+          Uri.parse('$_apiURL/download?downloadPageURL=$downloadPageURL'),
+        );
+      } else {
+        return await http.get(Uri.parse('$_apiURL/download?md5=$md5'));
+      }
     } catch (e) {
       return e;
     }
