e968454b28295871a1a5250d26e0606217ff2bbb
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-01 18:12:49 -0400

    split download events between sci-tech and fiction

--- lib/blocs/events/download_event.dart
+++ lib/blocs/events/download_event.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/widgets.dart';
+import 'package:libgen/domain/book_fiction_model.dart';
 
-import 'package:libgen/domain/book_model.dart';
+import 'package:libgen/domain/book_sci_tech_model.dart';
+import 'package:libgen/domain/i_book_model.dart';
 
 class DownloadEvent {
-  const DownloadEvent();
+  final BookModel book;
+  const DownloadEvent([this.book]);
 }
 
-class DownloadBookEvent extends DownloadEvent {
-  final BookModel book;
-  const DownloadBookEvent(this.book);
+class SciTechDownloadBookEvent extends DownloadEvent {
+  final BookSciTechModel book;
+  const SciTechDownloadBookEvent(this.book) : super(book);
+}
+
+class FictionDownloadBookEvent extends DownloadEvent {
+  final BookFictionModel book;
+  const FictionDownloadBookEvent(this.book) : super(book);
 }
 
 class DownloadFromBrowser extends DownloadEvent {
