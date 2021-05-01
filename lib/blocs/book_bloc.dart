28835629c0f3037662c87c6b2a7e053da2ace9c2
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-01 18:08:20 -0400

    split book bloc logic to fetch from sci-tech or fiction

--- lib/blocs/book_bloc.dart
+++ lib/blocs/book_bloc.dart
@@ -5,7 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 
-import 'package:libgen/domain/book_model.dart';
+import 'package:libgen/domain/book_fiction_model.dart';
+import 'package:libgen/domain/book_sci_tech_model.dart';
 import 'package:libgen/data/book_repository.dart';
 import 'events/book_events.dart';
 import 'states/book_states.dart';
@@ -19,7 +20,7 @@ class BookBloc extends Bloc<BookEvent, BookState> {
 
   @override
   Stream<BookState> mapEventToState(BookEvent event) async* {
-    if (event is BookFetchEvent) {
+    if (event is BookFetchSciTechEvent) {
       yield BookLoadingState(
         requiresCleaning: event.searchQuery.offset == 0,
       );
@@ -27,15 +28,47 @@ class BookBloc extends Bloc<BookEvent, BookState> {
           event.searchQuery.offset >= _totalCount) {
         yield BookNoMoreResults();
       } else {
-        final response = await bookRepository.getBooks(event.searchQuery);
+        final response =
+            await bookRepository.getSciTechBooks(event.searchQuery);
         if (response is http.Response) {
           if (response.statusCode == 200) {
             final rawResponse = jsonDecode(response.body);
             final rawBooks = rawResponse['data'] as List;
             _totalCount = rawResponse['totalCount'] as int;
-            final List<BookModel> books = [];
+            final List<BookSciTechModel> books = [];
             rawBooks.forEach((book) {
-              books.add(BookModel.fromJson(book));
+              books.add(BookSciTechModel.fromJson(book));
+            });
+            yield BookSuccessState(books: books, totalCount: _totalCount);
+          } else if (response.statusCode == 404) {
+            yield BookNoResultsState(searchTerm: event.searchQuery.searchTerm);
+          } else {
+            yield BookErrorState();
+          }
+        } else if (response is SocketException) {
+          yield BookConnectionFailed();
+        } else {
+          yield BookErrorState();
+        }
+      }
+    } else if (event is BookFetchFictionEvent) {
+      yield BookLoadingState(
+        requiresCleaning: event.searchQuery.offset == 0,
+      );
+      if (event.searchQuery.offset > 0 &&
+          event.searchQuery.offset >= _totalCount) {
+        yield BookNoMoreResults();
+      } else {
+        final response =
+            await bookRepository.getFictionBooks(event.searchQuery);
+        if (response is http.Response) {
+          if (response.statusCode == 200) {
+            final rawResponse = jsonDecode(response.body);
+            final rawBooks = rawResponse['data'] as List;
+            _totalCount = rawResponse['totalCount'] as int;
+            final List<BookFictionModel> books = [];
+            rawBooks.forEach((book) {
+              books.add(BookFictionModel.fromJson(book));
             });
             yield BookSuccessState(books: books, totalCount: _totalCount);
           } else if (response.statusCode == 404) {
