ff34abd1c93d6a2ffe38206530b20be5f7394f6c
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-22 23:55:21 -0400

    reorganize enums

--- lib/domain/filters_extensions.dart
+++
@@ -1,6 +0,0 @@
-export 'package:libgen/global/enums/extension.dart';
-export 'package:libgen/global/enums/language_enum.dart';
-export 'package:libgen/global/enums/reverse_order.dart';
-export 'package:libgen/global/enums/search_in.dart';
-export 'package:libgen/global/enums/sort_by.dart';
-export 'package:libgen/global/enums/wildcard_words.dart';
