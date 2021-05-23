a138751808a2c9b07b7af2895ea1a8f9832f5668
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-22 23:58:05 -0400

    (continued) refactor enums

---
+++ lib/domain/fiction_filters_extensions.dart
@@ -0,0 +1,3 @@
+export 'package:libgen/global/enums/fiction/extension.dart';
+export 'package:libgen/global/enums/fiction/search_in.dart';
+export 'package:libgen/global/enums/fiction/wildcard_words.dart';
