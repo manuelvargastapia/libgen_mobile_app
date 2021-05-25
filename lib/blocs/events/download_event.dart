5d5b5437e2bbd96d333da5b1b68b58e67e796909
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-25 12:48:50 -0400

    adapt DownloadEvents to work with download links

--- lib/blocs/events/download_event.dart
+++ lib/blocs/events/download_event.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/widgets.dart';
 import 'package:libgen/domain/book_fiction_model.dart';
-
 import 'package:libgen/domain/book_sci_tech_model.dart';
 import 'package:libgen/domain/i_book_model.dart';
 
 class DownloadEvent {
   final BookModel book;
-  const DownloadEvent([this.book]);
+  final String downloadLink;
+  const DownloadEvent([this.book, this.downloadLink]);
 }
 
 class SciTechDownloadBookEvent extends DownloadEvent {
   final BookSciTechModel book;
-  const SciTechDownloadBookEvent(this.book) : super(book);
+  final String downloadLink;
+  const SciTechDownloadBookEvent(this.book, this.downloadLink)
+      : super(book, downloadLink);
 }
 
 class FictionDownloadBookEvent extends DownloadEvent {
   final BookFictionModel book;
-  const FictionDownloadBookEvent(this.book) : super(book);
+  final String downloadLink;
+  const FictionDownloadBookEvent(this.book, this.downloadLink)
+      : super(book, downloadLink);
 }
 
 class DownloadFromBrowser extends DownloadEvent {
